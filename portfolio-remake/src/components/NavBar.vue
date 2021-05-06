bdbb2a1d9ed15c80bceba59f410a07ecfc34ec86
jmburton13 <jmburton13@gmail.com>
2021-05-06 14:23:00 -0400

    wired up global style sheet for color palette

--- portfolio-remake/src/components/NavBar.vue
+++ portfolio-remake/src/components/NavBar.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="navbar bg-light p-0 m-0">
+    <div class="navbar bg-eggshell p-0 m-0">
         <img class="navbar-brand ms-2" src="../assets/Jacob Burton Orbit Logo Infinite.gif" alt="the name Jacob Burton accompanied by the phrase 'A Fresh Perspective'" />
-        <nav>
+        <nav class="d-flex justify-content-between">
             <div class="dropdown me-3">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    Navigate
+                    Projects
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><router-link :to="{ name: 'about' }" class="text-lightblue p-0 m-0">About</router-link></li>
+                    <li><router-link :to="{ name: 'projects' }" class="text-lightblue p-0 m-0">Projects</router-link></li>
+                    <li><router-link :to="{ name: 'inspiration' }" class="text-lightblue p-0 m-0">Inspiration</router-link></li>
+                    <li><router-link :to="{ name: 'contact' }" class="text-lightblue p-0 m-0">Contact</router-link></li>
+                </ul>
+            </div>
+            <div class="dropdown me-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                    About
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
                     <li><router-link :to="{ name: 'about' }" class="text-lightblue p-0 m-0">About</router-link></li>
                     <li><router-link :to="{ name: 'projects' }" class="text-lightblue p-0 m-0">Projects</router-link></li>
                     <li><router-link :to="{ name: 'inspiration' }" class="text-lightblue p-0 m-0">Inspiration</router-link></li>
@@ -20,6 +31,9 @@
                 <router-link :to="{ name: 'contact' }" class="text-lightblue p-0 m-0">Contact</router-link>
             </div>
         </nav>
+        <div class="socials bg-darkblue w-100">
+            SOCIALS
+        </div>
     </div>
 </template>
 
@@ -30,8 +44,11 @@ export default {
 </script>
 
 <style scoped>
-.navbar {
+
+.socials {
+    height: 40px;
     border-bottom: solid 2px #5ffb72;
+    border-top: solid 2px #5ffb72;
 }
 
 img {
